5a3359653a0eef5ffc39be9a8cb44e21e414fad1
niejun <1289680419@qq.com>
2022-04-17 00:06:23 +0800

    提交2

---
+++ src/main/java/com/example/resolve/entities/Person.java
@@ -0,0 +1,39 @@
+package com.example.test;
+
+/**
+ * @author : niejun
+ * @Description: TODO
+ * @date Date : 2022年04月16日 16:32
+ **/
+public class Person {
+
+    public String name;
+
+    public Integer age;
+
+    public String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
